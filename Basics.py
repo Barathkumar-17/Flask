3591a23f8db8a717462d748f42ef26df2648a1f1
Barathkumar <barathkumar2k7@gmail.com>
2026-07-05 01:08:03 +0530

    Updated PUT for sql injection

--- Basics.py
+++ Basics.py
@@ -19,7 +19,7 @@ def get_expenses():
 
 @app.route('/expenses/<int:expense_id>')
 def get_expense(expense_id):
-    curr.execute(f'select * from expenses where id = ?;',(expense_id,))
+    curr.execute('select * from expenses where id = ?;',(expense_id,))
     row=curr.fetchone()
     if(row):
         return jsonify(f"id : {row[0]}, amount : {row[1]} , category : {row[2]}, date : {row[3]}")
@@ -28,20 +28,20 @@ def get_expense(expense_id):
 @app.route('/expenses',methods=['POST'])
 def add_expense():
     data=request.get_json();
-    curr.execute(f"insert into expenses values ((SELECT IFNULL(MAX(id), 0) + 1 FROM expenses),?,?,?);",
+    curr.execute("insert into expenses values ((SELECT IFNULL(MAX(id), 0) + 1 FROM expenses),?,?,?);",
                     (data["amount"],data["category"],data["date"],))
     #note there is no '?' rather just ? as it acts a simple placeholder instead of formatting the string into the string
     # the select iF null finds last id and then +1 .... 
     con.commit()
-    curr.execute(f"select * from expenses where id= last_insert_rowid();")
+    curr.execute("select * from expenses where id= last_insert_rowid();")
     expense=curr.fetchone()
     return jsonify(expense),201;
 
 @app.route('/expenses/<int:expense_id>',methods=['PUT'])
 def update_expense(expense_id):
     data=request.get_json()
-    curr.execute(f"""update expenses set amount={data["amount"]},category='{data["category"]}',date='{data["date"]}'
-                     where id={expense_id};""")
+    curr.execute("""update expenses set amount=?,category=?,date=?where id=?;""",
+                     (data["amount"],data["category"],data["date"],expense_id))
     con.commit()
     if(curr.rowcount==1):
             curr.execute(f'select * from expenses where id = {expense_id};')
